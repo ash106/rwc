f93dcca6725323fd4c664f9b7a978b7e4346e635
Alex Howington <alex.howington@gmail.com>
2014-06-26 00:09:53 -0600

    Comment out RecipientInterceptor code in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,8 @@
 require_relative 'production'
 
-Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch('EMAIL_RECIPIENTS'))
-)
+# Mail.register_interceptor(
+#   RecipientInterceptor.new(ENV.fetch('EMAIL_RECIPIENTS'))
+# )
 
 Rails.application.configure do
   # ...
