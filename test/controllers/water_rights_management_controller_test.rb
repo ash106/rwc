40fb0b94c28049feb96ce9941abb31c35681f93c
Alex Howington <alex.howington@gmail.com>
2014-11-21 20:15:54 -0600

    Forgot to remove a couple unnecessary destroy calls

--- test/controllers/water_rights_management_controller_test.rb
+++ test/controllers/water_rights_management_controller_test.rb
@@ -11,7 +11,6 @@ class WaterRightsManagementControllerTest < ActionController::TestCase
     assert_includes assigns(:place_of_use_areas), water_right.place_of_use_areas.first
     assert_includes assigns(:point_of_diversions), water_right.point_of_diversions.first
     assert_response :success
-    user.destroy
   end
 
   test "get show_water_rights is successful for non-signed in user" do
@@ -26,7 +25,6 @@ class WaterRightsManagementControllerTest < ActionController::TestCase
     get :show_water_rights
     assert_equal user.id, assigns(:user_id)
     assert_response :success
-    user.destroy
   end
 
   test "get get_data is successful for admin" do
