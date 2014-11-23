28eb322a894703e22ff7dd1aaf42d8e336689580
Alex Howington <alex.howington@gmail.com>
2014-11-23 02:11:09 -0600

    Refactor WRM get_data action

--- app/controllers/water_rights_management_controller.rb
+++ app/controllers/water_rights_management_controller.rb
@@ -19,23 +19,25 @@ class WaterRightsManagementController < ApplicationController
   end
 
   def get_data
-    areas = []
-    points = []
-    if params[:id].to_i > 0
-      user = User.find(params[:id].to_i)
-      if user.has_role? :admin
-        areas = PlaceOfUseArea.all
-        points = PointOfDiversion.all
-      else
-        user.water_rights.each do |wr|
-          areas << wr.place_of_use_areas
-          points << wr.point_of_diversions
-        end
-        areas = areas.flatten.uniq{|a| a.id}
-        points = points.flatten.uniq{|p| p.id}
-      end
+    if params[:id].to_i > 0 # params[:id] is set to 0 if no user signed in
+      user = User.find(params[:id])
+      @geometry = set_geometry(user)
     else
       user = User.find_by(email: 'tester@example.com')
+      @geometry = set_geometry(user)
+    end
+    render json: @geometry, root: "features", meta: "FeatureCollection", meta_key: 'type'
+  end
+
+private
+
+  def set_geometry(user)
+    areas = []
+    points = []
+    if user.has_role? :admin
+      areas = PlaceOfUseArea.all
+      points = PointOfDiversion.all
+    else
       user.water_rights.each do |wr|
         areas << wr.place_of_use_areas
         points << wr.point_of_diversions
@@ -43,7 +45,7 @@ class WaterRightsManagementController < ApplicationController
       areas = areas.flatten.uniq{|a| a.id}
       points = points.flatten.uniq{|p| p.id}
     end
-    @geometry = areas + points
-    render json: @geometry, root: "features", meta: "FeatureCollection", meta_key: 'type'
+    areas + points
   end
+
 end
