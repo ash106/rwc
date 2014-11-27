55862fabce0127bc1f888c9a9b7c337ca18d4189
Alex Howington <alex.howington@gmail.com>
2014-11-26 22:04:05 -0600

    Refactor WRM controller duplication issues

--- app/controllers/water_rights_management_controller.rb
+++ app/controllers/water_rights_management_controller.rb
@@ -5,8 +5,8 @@ class WaterRightsManagementController < ApplicationController
     if params[:user_id]
       @user = User.find(params[:user_id])
       @water_rights = @user.water_rights
-      @place_of_use_areas = set_areas(@user)
-      @point_of_diversions = set_points(@user)
+      @place_of_use_areas = set_geometry(@user, 'place_of_use_areas')
+      @point_of_diversions = set_geometry(@user, 'point_of_diversions')
     else
       @water_rights = WaterRight.all
       @place_of_use_areas = PlaceOfUseArea.all
@@ -32,34 +32,25 @@ class WaterRightsManagementController < ApplicationController
   def get_data
     if params[:id].to_i > 0 # params[:id] is set to 0 if no user signed in
       user = User.find(params[:id])
-      if user.has_role? :admin
-        @geometry = PlaceOfUseArea.all + PointOfDiversion.all
-      else
-        @geometry = set_areas(user) + set_points(user)
-      end
     else
       user = User.find_by(email: 'tester@example.com')
-      @geometry = set_areas(user) + set_points(user)
+    end
+    if user.has_role? :admin
+      @geometry = PlaceOfUseArea.all + PointOfDiversion.all
+    else
+      @geometry = set_geometry(user, 'place_of_use_areas') + set_geometry(user, 'point_of_diversions')
     end
     render json: @geometry, root: "features", meta: "FeatureCollection", meta_key: 'type'
   end
 
 private
 
-  def set_areas(user)
-    areas = []
+  def set_geometry(user, geo_type)
+    geometry = []
     user.water_rights.each do |wr|
-      areas << wr.place_of_use_areas
+      geometry << wr.send(geo_type)
     end
-    areas.flatten.uniq{|a| a.id}
-  end
-
-  def set_points(user)
-    points = []
-    user.water_rights.each do |wr|
-      points << wr.point_of_diversions
-    end
-    points.flatten.uniq{|p| p.id}
+    geometry.flatten.uniq{|a| a.id}
   end
 
 end
