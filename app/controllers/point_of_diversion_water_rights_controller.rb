272e4d88bec51642af53267e8bc45a85e01e5208
Alex Howington <alex.howington@gmail.com>
2014-12-20 22:35:50 -0600

    Add destroy action to point_of_diversion_water_rights

--- app/controllers/point_of_diversion_water_rights_controller.rb
+++ app/controllers/point_of_diversion_water_rights_controller.rb
@@ -10,6 +10,12 @@ class PointOfDiversionWaterRightsController < ApplicationController
     redirect_to water_right_path(params[:point_of_diversion_water_right][:water_right_id])
   end
 
+  def destroy
+    point_of_diversion_water_right = PointOfDiversionWaterRight.find_by(point_of_diversion_id: params[:point_of_diversion_id], water_right_id: params[:water_right_id])
+    point_of_diversion_water_right.destroy
+    redirect_to water_right_path(params[:water_right_id]), notice: 'Association was successfully destroyed.'
+  end
+
 private
 
   def point_of_diversion_water_right_params
