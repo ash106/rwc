d0994fdbd2ab45be89b30b5232a9efb69ec5c4cd
Alex Howington <alex.howington@gmail.com>
2017-05-07 15:39:35 -0600

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ user = User.create!(email: 'user@example.com', password: 'changeme')
 user.add_role :customer
 this_guy = User.create!(email: 'tester@example.com', password: 'changeme')
 this_guy.add_role :customer
-logan = User.create!(email: 'rileylog@gmail.com', password: 'password')
+logan = User.create!(email: 'riley@gmail.com', password: 'password')
 logan.add_role :customer
-itsamemario = User.create!(email: 'alex.howington@gmail.com', password: 'wheatbox')
+itsamemario = User.create!(email: 'alex@gmail.com', password: 'wheatbox')
 itsamemario.add_role :admin
 admin = User.create!(email: 'admin@rwc.com', password: 'loganlogan')
 admin.add_role :admin
@@ -74,20 +74,20 @@ well_two.save!
 # PointOfDiversion.parse_kml(well_two.id)
 
 
-water_right = WaterRight.create!(number: '13-142', flow_cfs: 5.12, flow_ac_ft: 1860, sole_supply_acres: 620, place_of_use: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-142', flow_cfs: 5.12, flow_ac_ft: 1860, uses: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: stream.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: well.id)
-water_right = WaterRight.create!(number: '13-146', flow_cfs: 8.7, flow_ac_ft: 2716.5, sole_supply_acres: 905.5, place_of_use: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-146', flow_cfs: 8.7, flow_ac_ft: 2716.5, uses: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: well.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: well_two.id)
-water_right = WaterRight.create!(number: '13-147', flow_cfs: 2.31, flow_ac_ft: 480, sole_supply_acres: 160, place_of_use: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-147', flow_cfs: 2.31, flow_ac_ft: 480, uses: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
 water_right.place_of_use_area_water_rights.create!(place_of_use_area_id: area_one.id)
-water_right = WaterRight.create!(number: '13-155', flow_cfs: 7.4, flow_ac_ft: 0, sole_supply_acres: 0, place_of_use: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-155', flow_cfs: 7.4, flow_ac_ft: 0, uses: 'Full Farm', change_application_number: 'a28264', proof_due_date: "2016-06-30", priority_date: "1908-10-27", user_id: this_guy.id)
 water_right.place_of_use_area_water_rights.create!(place_of_use_area_id: area_two.id)
-water_right = WaterRight.create!(number: '13-198', flow_cfs: 1.11, flow_ac_ft: 475.08, sole_supply_acres: 179.8, place_of_use: 'Sec 19 & 20', proof_due_date: "2015-05-31", priority_date: "1972-07-17", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-198', flow_cfs: 1.11, flow_ac_ft: 475.08, uses: 'Sec 19 & 20', proof_due_date: "2015-05-31", priority_date: "1972-07-17", user_id: this_guy.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: stream.id)
 water_right.place_of_use_area_water_rights.create!(place_of_use_area_id: area_three.id)
-water_right = WaterRight.create!(number: '13-1138', flow_cfs: 10, flow_ac_ft: 1920, sole_supply_acres: 640, place_of_use: 'Most Farm', change_application_number: 'a28259', proof_due_date: "2015-05-31", priority_date: "1972-07-17", user_id: this_guy.id)
+water_right = WaterRight.create!(number: '13-1138', flow_cfs: 10, flow_ac_ft: 1920, uses: 'Most Farm', change_application_number: 'a28259', proof_due_date: "2015-05-31", priority_date: "1972-07-17", user_id: this_guy.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: well.id)
 water_right.place_of_use_area_water_rights.create!(place_of_use_area_id: area_one.id)
 
@@ -140,7 +140,7 @@ eleven.kml = File.open(File.join(Rails.root, "/test/fixtures/57-3411_11.kml"))
 eleven.save!
 # PointOfDiversion.parse_kml(eleven.id)
 
-water_right = WaterRight.create!(number: '57-3411', priority_date: "1880-01-01", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_ac_ft: 7040, place_of_use: 'Municipal', comments: 'Board of Water Resources on this change', user_id: logan.id)
+water_right = WaterRight.create!(number: '57-3411', priority_date: "1880-01-01", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_ac_ft: 7040, uses: 'Municipal', comments: 'Board of Water Resources on this change', user_id: logan.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: one.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: three.id)
@@ -152,7 +152,7 @@ water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: eight
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: nine.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: ten.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: eleven.id)
-water_right = WaterRight.create!(number: '57-443', priority_date: "1940-09-05", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_cfs: 1.4, place_of_use: 'Municipal', user_id: logan.id)
+water_right = WaterRight.create!(number: '57-443', priority_date: "1940-09-05", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_cfs: 1.4, uses: 'Municipal', user_id: logan.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: one.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: three.id)
@@ -164,7 +164,7 @@ water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: eight
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: nine.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: ten.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: eleven.id)
-water_right = WaterRight.create!(number: '57-10191', priority_date: "1908-10-27", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_ac_ft: 2465.36, place_of_use: 'Municipal', user_id: logan.id)
+water_right = WaterRight.create!(number: '57-10191', priority_date: "1908-10-27", change_application_number: 'a37915', proof_due_date: "2017-04-30", flow_ac_ft: 2465.36, uses: 'Municipal', user_id: logan.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: one.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: three.id)
@@ -195,14 +195,13 @@ two_four.kml = File.open(File.join(Rails.root, "/test/fixtures/57-10327_4.kml"))
 two_four.save!
 # PointOfDiversion.parse_kml(two_four.id)
 
-water_right = WaterRight.create!(number: '57-10327', priority_date: "1972-06-13", change_application_number: 'a37441', proof_due_date: "2022-09-30", flow_ac_ft: 23.4, place_of_use: 'Municipal', comments: "Proof due date is end of 50-yr period; seg'd from 57-7833", user_id: logan.id)
+water_right = WaterRight.create!(number: '57-10327', priority_date: "1972-06-13", change_application_number: 'a37441', proof_due_date: "2022-09-30", flow_ac_ft: 23.4, uses: 'Municipal', comments: "Proof due date is end of 50-yr period; seg'd from 57-7833", user_id: logan.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_one.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_two.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_three.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_four.id)
-water_right = WaterRight.create!(number: '57-7839', priority_date: "1972-07-17", change_application_number: 'a38167', proof_due_date: "2017-08-31", flow_ac_ft: 25.856, place_of_use: 'Municipal', user_id: logan.id)
+water_right = WaterRight.create!(number: '57-7839', priority_date: "1972-07-17", change_application_number: 'a38167', proof_due_date: "2017-08-31", flow_ac_ft: 25.856, uses: 'Municipal', user_id: logan.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_one.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_two.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_three.id)
 water_right.point_of_diversion_water_rights.create!(point_of_diversion_id: two_four.id)
-
