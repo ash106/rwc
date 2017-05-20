b2d2c2de3a714301e5623bb6bd09b6f8421d2d0e
Alex Howington <alex.howington@gmail.com>
2017-05-20 17:31:33 -0500

    Cleanup whitespace in water right serializer

--- app/serializers/water_right_serializer.rb
+++ app/serializers/water_right_serializer.rb
@@ -1,4 +1,3 @@
 class WaterRightSerializer < ActiveModel::Serializer
   attributes :number, :flow_cfs, :flow_ac_ft, :sole_supply_acres, :uses, :change_application_number, :proof_due_date, :priority_date, :comments
-
 end
