3ba8b7752fd5086e1d233aaa9eb6515d50541a16
Alex Howington <alex.howington@gmail.com>
2017-05-21 19:22:48 -0500

    Add validations to join tables

--- app/models/place_of_use_area_water_right.rb
+++ app/models/place_of_use_area_water_right.rb
@@ -1,5 +1,5 @@
 class PlaceOfUseAreaWaterRight < ActiveRecord::Base
   belongs_to :place_of_use_area
   belongs_to :water_right
-  validates :place_of_use_area, presence: true
+  validates :place_of_use_area, :water_right, presence: true
 end
