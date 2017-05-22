249279607576b2c25a03b52c5a41f4f754b00070
Alex Howington <alex.howington@gmail.com>
2017-05-21 22:42:00 -0500

    Remove WaterRightSerializer until I figure out how to use it with other serializers

--- app/serializers/water_right_serializer.rb
+++
@@ -1,3 +0,0 @@
-class WaterRightSerializer < ActiveModel::Serializer
-  attributes :number, :flow_cfs, :flow_ac_ft, :sole_supply_acres, :uses, :change_application_number, :proof_due_date, :priority_date, :comments
-end
